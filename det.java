46c40efb1ed6146f04bda2ae3c6dd2226f8a65e9
siewertsen1999 <73901609+siewertsen1999@users.noreply.github.com>
2021-05-17 18:28:20 +0200

    Update det.java

--- det.java
+++ det.java
@@ -1,10 +1,5 @@
 package Determinanten;
-/*
-@authors:
-Nguyen, To Uyen 574330
-Siewertsen, Paul 572933
-Valentin, Leonard 5756986
- */
+
 public class det {
     public static int nrOfMult;
     public static int nrOfDiv;
@@ -16,15 +11,7 @@ public class det {
         }
         return reti;
     }
-    /*
-    dies ist eine rekursive Implementation der Determinanten Berechnung nach Vorlage im Skript
-    https://moodle.htw-berlin.de/pluginfile.php/985008/mod_resource/content/3/TEXTL41neu.pdf :
-    die Abbruchbedingung ist eine Matrix der groesse 1. Wenn dies gegeben ist gibt  der code das einzige Element im Array zurueck,
-    was in dem Fall die Determinante ist. im anderen fall wird die die Determinaten-Funktion rekursiv aufgerufen, mit einer kleineren matrix, in der
-    doe oberste Zeile, und die Spalte, die durch die Lauf-varaible i angegben wird, entefernt sind( i itereriert uber die gesamte erste Zeile).
-    diese werden alternierend auf den return value addiert und subrahiert
-
-     */
+    
     public static double calcDetRekursiv(double[][] A){
         double det = 0;
         if(A.length == 1){
@@ -56,12 +43,12 @@ public class det {
             for(int s=0;s<spalten;s++)
             {
                 if(a[z][s]!=0) {
-                    // Wenn die nicht 0 ist, springen wir aus der Schleife
+                    
                     allesNull = false;
                     break;
                 }
             }
-            if(allesNull==true) // Wir tauschen die Zeile mit der 0 in die letzte Zeile der Matrix
+            if(allesNull==true)
             {
                 zeilentausch(a, z, zeilen-1);
                 zeilen--;
@@ -82,11 +69,11 @@ public class det {
                 zeilentausch(a,p,p+z);
                 z++;
             }
-            for(z=1;z<zeilen-p;z++) // Wir gehen die Zeilen unter der ersten durch
+            for(z=1;z<zeilen-p;z++) 
             {
-                if(a[p+z][p]!=0)//Wenn a[p+z][p] nicht 0 ist
+                if(a[p+z][p]!=0)
                 {
-                    //Faktor zu Bildung des Minuenden, um an der Stelle a[p+z][p] eine 0 zu bilden
+                    
                     double x = -a[p+z][p]/a[p][p];
                     nrOfDiv++;
                     for(int s=p;s<spalten;s++)
@@ -96,13 +83,10 @@ public class det {
                     }
                 }
             }
-            p++;//Wir gehen in die nächste Spalte
+            p++;
         }
     }
-    /*
-    dies ist eine Helferfunktion fuer die rekursive Determinantenberechnug. Sie entfernt einfach die durch x angegebene spalte
-    ein angeben der zu entfernenden zeile ist nicht noetig, da fuer unsere funktion immer die kopfzeile entfernt wird
-     */
+   
     public static double[][] removeRows(double[][] A, int x){
         double[][] ret = new double[A.length - 1][A.length - 1];
         int offset = 0;
